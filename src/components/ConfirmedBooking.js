ff5112629a05f13e96db7aee5a03543a39941746
AryanSuvarna <aryansat@hotmail.com>
2024-04-27 18:16:31 -0400

    updated confirmation page

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 const ConfirmedBooking = () => {
     return (
-        <div className='confirm'>
-            <div>
-                <h1 className="comfirm">
+        <div>
+            <div className="confirm">
+                <h1 >
                     Booking has been <span>confirmed!</span>
                 </h1>
+                <h3 style={{"font-style": "italic"}}>We can't wait to see you!</h3>
             </div>
         </div>
     )
